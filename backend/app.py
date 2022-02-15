d89b67ac88fc9550b602abd87d8ab75d18621a7d
CoffeeVictor <victor@manifestohut.com.br>
2022-02-15 11:38:02 -0300

    Add cors

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,22 @@
-from flask import Flask, request
+from flask import Flask, make_response, request
+from flask_cors import CORS
 
 from src.libs.morse import morse_to_str
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods = ['POST'])
 def hw():
     json = request.get_json()
     message = json['message']
 
-    return {
-        'plain_text_message': morse_to_str(message)
-    }
+    try:
+      return {
+            'plain_text_message': morse_to_str(message)
+        }
+    except KeyError:
+        return make_response('Invalid Morse Code', 400)
 
 
 app.run(debug=True)
